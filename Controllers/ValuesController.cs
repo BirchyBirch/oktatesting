5b083ec3777b31ce202ddd513329f6593a37c0af
Chris Burchsted <chris.burchsted@gmail.com>
2019-10-30 21:21:37 -0500

    commit of project

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace okta_experiments.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public ValuesController(IConfiguration values)
+        {
+            Values = values;
+        }
+
+        public IConfiguration Values { get; }
+
+
+        // GET api/values/secure
+        [Authorize]
+        [HttpGet]
+        [Route("secure")]
+        public ActionResult<IEnumerable<string>> GetSecure()
+        {
+            var section = Values.GetSection("ReturnValues");
+            var data= new List<string>();
+            section.Bind(data);
+            return data;
+        }
+
+        // GET api/values/insecure
+        [AllowAnonymous]
+        [HttpGet]
+        [Route("insecure")]
+        public ActionResult<IEnumerable<string>> GetInsecure()
+        {
+            var section = Values.GetSection("ReturnValues_IN");
+            var data = new List<string>();
+            section.Bind(data);
+            return data;
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Okta.AspNetCore;
+
+namespace okta_experiments
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            bool enableAuth = Configuration.GetValue<bool>("EnableAuth");
+            if (enableAuth)
+            {
+                services.AddAuthentication(options =>
+                {
+                    options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                    options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = OktaDefaults.MvcAuthenticationScheme;
+                })
+                .AddCookie()
+                .AddOktaMvc(new OktaMvcOptions
+                {
+                    OktaDomain = Configuration.GetValue<string>("OktaDomain"),
+                    ClientId = Configuration.GetValue<string>("ClientId"),
+                    ClientSecret = Configuration.GetValue<string>("ClientSecret")
+                });
+            }
+
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            //app.UseHttpsRedirection();
+            if (Configuration.GetValue<bool>("EnableAuth"))
+            {
+                app.UseAuthentication();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
